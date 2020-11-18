e7b9e9ece7d56ef447b3dd6ca9924f0a3664f495
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2020-11-18 15:20:12 +0100

    added cors headers

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+app.use(cors());
 
 const comments = [{ id: 1, author: 'Wilder', message: 'Super !', rating: 4 }];
 
